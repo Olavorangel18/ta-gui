c0c42d49bb7f633a4ad7a18eed0f844a0ab2203a
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:16:32 -0300

    visualizacao da lista de alunos cadastrados

--- ta-gui/src/app/aluno.ts
+++ ta-gui/src/app/aluno.ts
@@ -3,4 +3,11 @@ export class Aluno {
   cpf: string;
   email: string;
   github:string;
+
+  constructor(nome:string, cpf:string, email:string, github:string){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.github = github;
+  }
 }
