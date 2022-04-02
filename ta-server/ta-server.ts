pauloborba/teachingassistant

servidor integrado ao cliente

server/ta-server/ta-server.ts
@@ -6,18 +6,26 @@ import {CadastroDeAlunos} from './cadastrodealunos';
 
 var app = express();
 
-var alunos: CadastroDeAlunos = new CadastroDeAlunos();
+var cadastro: CadastroDeAlunos = new CadastroDeAlunos();
+
+var allowCrossDomain = function(req: any, res: any, next: any) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
 
 app.use(bodyParser.json());
 
 app.get('/alunos', function (req, res) {
-  var aluno: string = JSON.stringify(alunos.getAlunos());
-  res.send(aluno);
+  console.log('GET /alunos: ' + req)
+  res.send(JSON.stringify(cadastro.getAlunos()));
 })
 
 app.post('/aluno', function (req: express.Request, res: express.Response) {
   var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
-  aluno = alunos.criar(aluno);
+  aluno = cadastro.criar(aluno);
   if (aluno) {
     res.send({"success": "O aluno foi cadastrado com sucesso"});
   } else {
@@ -27,7 +35,7 @@ app.post('/aluno', function (req: express.Request, res: express.Response) {
 
 app.put('/aluno', function (req: express.Request, res: express.Response) {
   var aluno: Aluno = <Aluno> req.body;
-  aluno = alunos.atualizar(aluno);
+  aluno = cadastro.atualizar(aluno);
   if (aluno) {
     res.send({"success": "O aluno foi atualizado com sucesso"});
   } else {

Olavorangel18/ta-gui

ta-server/ta-server.ts
@@ -6,33 +6,39 @@ import {CadastroDeAlunos} from './cadastrodealunos';
 
 var taserver = express();
 
-var alunos: CadastroDeAlunos = new CadastroDeAlunos();
+var cadastro: CadastroDeAlunos = new CadastroDeAlunos();
+
+var allowCrossDomain = function(req: any, res: any, next: any) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+taserver.use(allowCrossDomain);
 
 taserver.use(bodyParser.json());
 
-taserver.get('/alunos', function (req, res) {
-  var aluno: string = JSON.stringify(alunos.getAlunos());
-  console.log(aluno)
-  res.send(aluno);
+taserver.get('/alunos', function (req: express.Request, res: express.Response) {
+  res.send(JSON.stringify(cadastro.getAlunos()));
 })
 
 taserver.post('/aluno', function (req: express.Request, res: express.Response) {
-  var aluno: Aluno = <Aluno> req.body; //verificar se é mesmo Aluno!
-  aluno = alunos.criar(aluno);
+  var aluno: Aluno = <Aluno> req.body; //verificar se � mesmo Aluno!
+  aluno = cadastro.cadastrar(aluno);
   if (aluno) {
     res.send({"success": "O aluno foi cadastrado com sucesso"});
   } else {
-    res.send({"failure": "O aluno não pode ser cadastrado"});
+    res.send({"failure": "O aluno n�o pode ser cadastrado"});
   }
 })
 
 taserver.put('/aluno', function (req: express.Request, res: express.Response) {
   var aluno: Aluno = <Aluno> req.body;
-  aluno = alunos.atualizar(aluno);
+  aluno = cadastro.atualizar(aluno);
   if (aluno) {
     res.send({"success": "O aluno foi atualizado com sucesso"});
   } else {
-    res.send({"failure": "O aluno não pode ser atualizado"});
+    res.send({"failure": "O aluno n�o pode ser atualizado"});
   }
 })
 
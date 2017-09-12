4885f5209460385dd20d50e0e107dfdb962d6b54
pauloborba <phmb@cin.ufpe.br>
2017-09-12 20:51:20 -0300

    servidor integrado ao cliente

--- ta-gui/src/app/alunos.component.ts
+++ ta-gui/src/app/alunos.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgModule } from '@angular/core';
-
 import { Aluno } from '../../../common/aluno';
 import { AlunoService } from './aluno.service';
 
@@ -10,27 +8,38 @@ import { AlunoService } from './aluno.service';
    styleUrls: ['./alunos.component.css']
  })
  export class AlunosComponent implements OnInit {
-    constructor(private alunoService: AlunoService) {}
 
     aluno: Aluno = new Aluno();
-    alunos: Aluno[] | undefined;
+    alunos: Aluno[] = [];
     cpfduplicado: boolean = false;
 
+    constructor(private alunoService: AlunoService) {}
+
      criarAluno(a: Aluno): void {
-      if (this.alunoService.criar(a) && this.alunos) {
-        this.alunos.push(a);
-        this.aluno = new Aluno();
-      } else {
-        this.cpfduplicado = true;
-      }
+       this.alunoService.criar(a)
+              .subscribe(
+                ar => {
+                  if (ar) {
+                    this.alunos.push(ar);
+                    this.aluno = new Aluno();
+                  } else {
+                    this.cpfduplicado = true;
+                  }
+                },
+                msg => { alert(msg.message); }
+              );
     }
 
-     onMove(): void {
+    onMove(): void {
        this.cpfduplicado = false;
     }
 
      ngOnInit(): void {
-      this.alunos = this.alunoService.getAlunos();
-    }
+       this.alunoService.getAlunos()
+             .subscribe(
+               as => { this.alunos = as; },
+               msg => { alert(msg.message); }
+              );
+     }
 
   }
